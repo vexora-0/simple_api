6b3d9d265f2725bbfde658ed33deee8f692ad85c
Bhargav <143892094+vexora-0@users.noreply.github.com>
2024-12-20 12:00:00 +0000

    add

---
+++ src/main/java/com/egov/demo_egov/DemoEgovApplication.java
@@ -0,0 +1,13 @@
+package com.egov.demo_egov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEgovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEgovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/egov/demo_egov/DemoEgovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egov.demo_egov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEgovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
