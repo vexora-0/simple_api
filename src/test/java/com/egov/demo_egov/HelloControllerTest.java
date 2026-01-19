1d93cd924ae68f0530ea5dd8ee49d5afff650395
Bhargav <bhargav4g13132@gmail.com>
2026-01-19 16:31:26 +0530

    Import HelloController in HelloControllerTest.java to enable testing of controller functionalities

--- src/test/java/com/egov/demo_egov/HelloControllerTest.java
+++ src/test/java/com/egov/demo_egov/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.egov.demo_egov;
 
+import com.egov.demo_egov.controller.HelloController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
